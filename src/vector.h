2b8f5dda085b7e98853fc630f44c82633dd77fcd
kinson261 <clarkrazananirina13@gmail.com>
2026-01-23 16:48:54 +0300

    [FIX] Used template specialization instead of inheritance. As a consequence, we get compile time errors instead of runtime errors

--- src/vector.h
+++ src/vector.h
@@ -1,59 +1,99 @@
 #include "point2D.h"
 #include "point3D.h"
 #include "point3D4.h"
+#include <cstddef>
 #include <optional>
 #include <stdexcept>
 
-template <typename T>
-class Vector
+template <typename T, std::size_t N>
+requires ((N>=2) && (N<=4))
+class Vector;
+
+template<typename T>
+class Vector<T,4>
 {
     private:
         T dx;
         T dy;
-        std::optional<T> dz;
-        std::optional<T> scale;
+        T dz;
+        T scale;
 
     public:
     // Default constructor
-    Vector<T>() : dx(), dy () , dz () {};
-    Vector<T>(T x, T y) : dx(x), dy (y) , dz (std::nullopt), scale(std::nullopt) {};
-    Vector<T>(T x, T y, T z) : dx(x), dy (y) , dz (z) {};
-    Vector<T>(T x, T y, T z, T s) : dx(x), dy (y) , dz (z), scale(s) {};
+    Vector() : dx(), dy () , dz (), scale() {};
+    Vector(T x, T y, T z) : dx(x), dy (y) , dz (z), scale(1) {};
+    Vector(T x, T y, T z, T s) : dx(x), dy (y) , dz (z), scale(s) {};
 
     // Destructor
     virtual ~Vector() {}
 
-    //Constructor from Points of different types
+    // Constructor with points
     template<typename U, typename V>
-    requires (std::integral<U> || std::floating_point<U>) || (std::integral<V> || std::floating_point<V>)
-    Vector<T>(const Point2D<U>& pt1, const Point2D<V>& pt2): dz(std::nullopt), scale(std::nullopt)
+    requires (std::integral<U> || std::floating_point<U>) && (std::integral<V> || std::floating_point<V>)
+    Vector(const Point3D4<U>& pt1, const Point3D4<V>& pt2)
     {
         auto pt1_coord = pt1.get();
         auto pt2_coord = pt2.get();
         dx = static_cast<T>(pt2_coord.at(0) - pt1_coord.at(0));
         dy = static_cast<T>(pt2_coord.at(1) - pt1_coord.at(1));
+        dz = static_cast<T>(pt2_coord.at(2) - pt1_coord.at(2));
+        pt2_coord.at(3) != pt1_coord.at(3) ? throw std::invalid_argument("Scale must be the same.") : scale = pt1_coord.at(3);
     }
+};
 
+template<typename T>
+class Vector<T,3>
+{
+    private:
+        T dx;
+        T dy;
+        T dz;
+
+    public:
+    // Default constructor
+    Vector() : dx(), dy () , dz () {};
+    Vector(T x, T y, T z) : dx(x), dy (y) , dz (z){};
+
+    // Destructor
+    virtual ~Vector() {}
+
+    // Constructor with points
     template<typename U, typename V>
-    requires (std::integral<U> || std::floating_point<U>) || (std::integral<V> || std::floating_point<V>)
-    Vector<T>(const Point3D<U>& pt1, const Point3D<V>& pt2): scale(std::nullopt)
+    requires (std::integral<U> || std::floating_point<U>) && (std::integral<V> || std::floating_point<V>)
+    Vector(const Point3D<U>& pt1, const Point3D<V>& pt2)
     {
         auto pt1_coord = pt1.get();
         auto pt2_coord = pt2.get();
         dx = static_cast<T>(pt2_coord.at(0) - pt1_coord.at(0));
         dy = static_cast<T>(pt2_coord.at(1) - pt1_coord.at(1));
-        dy = static_cast<T>(pt2_coord.at(2) - pt1_coord.at(2));
+        dz = static_cast<T>(pt2_coord.at(2) - pt1_coord.at(2));
     }
+};
 
+
+template<typename T>
+class Vector<T,2>
+{
+    private:
+        T dx;
+        T dy;
+
+    public:
+    // Default constructor
+    Vector() : dx(), dy () {};
+    Vector(T x, T y) : dx(x), dy (y) {};
+
+    // Destructor
+    virtual ~Vector() {}
+
+    // Constructor with points
     template<typename U, typename V>
-    requires (std::integral<U> || std::floating_point<U>) || (std::integral<V> || std::floating_point<V>)
-    Vector<T>(const Point3D4<U>& pt1, const Point3D4<V>& pt2): scale(std::nullopt)
+    requires (std::integral<U> || std::floating_point<U>) && (std::integral<V> || std::floating_point<V>)
+    Vector(const Point2D<U>& pt1, const Point2D<V>& pt2)
     {
         auto pt1_coord = pt1.get();
         auto pt2_coord = pt2.get();
-        pt2_coord.at(3) != pt1_coord.at(3) ? throw std::invalid_argument("Scale must be the same.") : true;
         dx = static_cast<T>(pt2_coord.at(0) - pt1_coord.at(0));
         dy = static_cast<T>(pt2_coord.at(1) - pt1_coord.at(1));
-        dy = static_cast<T>(pt2_coord.at(2) - pt1_coord.at(2));
     }
 };
